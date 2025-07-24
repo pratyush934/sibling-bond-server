c056568349abe5e651cd4cc94157509aeb569302
Pratyush <oggycandle786@gmail.com>
2025-07-24 23:49:41 +0530

    Adding

--- models/category_model.go
+++ models/category_model.go
@@ -1,6 +1,11 @@
 package models
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"github.com/pratyush934/sibling-bond-server/database"
+	"gorm.io/gorm"
+	"time"
+)
 
 type Category struct {
 	Id          string    `gorm:"primaryKey" json:"id"`
@@ -10,4 +15,39 @@ type Category struct {
 	Products    []Product `gorm:"foreignKey:CategoryId" json:"products,omitempty"`
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
+}
+
+/*
+Create(category *Category) (*Category, error)
+
+GetByID(id string) (*Category, error)
+
+GetByName(name string) (*Category, error)
+
+Update(category *Category) (*Category, error)
+
+Delete(id string) error
+
+GetAll() ([]*Category, error)
+
+*/
+
+func (c *Category) BeforeCreate(tx *gorm.DB) error {
+	c.Id = uuid.New().String()
+	return nil
+}
+
+func (c *Category) CreateCategory() (*Category, error) {
+	if err := database.DB.Create(c).Error; err != nil {
+		return &Category{}, err
+	}
+	return c, nil
+}
+
+func GetCategoryById(id string) (*Category, error) {
+	var category Category
+	if err := database.DB.Where(&Category{Id: id}).First(&category).Error; err != nil {
+		return &category, err
+	}
+	return &category, nil
 }
--- models/product_model.go
+++ models/product_model.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"github.com/google/uuid"
+	"github.com/pratyush934/sibling-bond-server/database"
+	"github.com/rs/zerolog/log"
+	"gorm.io/gorm"
+	"time"
+)
 
 type Product struct {
 	Id            string    `gorm:"primaryKey" json:"id"`
@@ -13,4 +19,67 @@ type Product struct {
 	ImageUrl      string    `json:"imageUrl"`
 	CreatedAt     time.Time `json:"createdAt"`
 	UpdatedAt     time.Time `json:"updatedAt"`
+}
+
+func (p *Product) BeforeCreate(t *gorm.DB) error {
+	p.Id = uuid.New().String()
+	return nil
+}
+
+func (p *Product) CreateProduct() (*Product, error) {
+	if err := database.DB.Create(p).Error; err != nil {
+		log.Err(err).Msg("Issue persist in the CreateProduct")
+		return &Product{}, err
+	}
+	return p, nil
+}
+
+func GetProductById(id string) (*Product, error) {
+	var product Product
+	if err := database.DB.Where(&Product{Id: id}).First(&product).Error; err != nil {
+		log.Err(err).Msg("Issue exist in GetProductById")
+		return &product, err
+	}
+	return &product, nil
+}
+
+func UpdateProduct(p *Product) (*Product, error) {
+	if err := database.DB.Updates(p).Error; err != nil {
+		log.Err(err).Msg("Issue exist in UpdateProduct")
+		return &Product{}, err
+	}
+	return p, nil
+}
+
+func DeleteProduct(id string) error {
+	return database.DB.Where(&Product{Id: id}).Delete(&Product{}).Error
+}
+
+func GetAllProducts(limit, offSet int, categoryId, searchQuery string) ([]Product, error) {
+	var products []Product
+	query := database.DB.Limit(limit).Offset(offSet)
+
+	if categoryId != "" {
+		query = query.Where(&Product{CategoryId: categoryId})
+	}
+
+	if searchQuery != "" {
+		query = query.Where("name ILIKE ? OR description ILIKE ?", "%"+searchQuery+"%", "%"+searchQuery+"%")
+	}
+
+	if err := query.Find(&products).Error; err != nil {
+		log.Err(err).Msg("Issue exist in GetAllProducts")
+		return nil, err
+	}
+
+	return products, nil
+
+}
+
+func UpdateStock(productId string, quantityChange int) error {
+	if err := database.DB.Model(&Product{}).Where("id = ?", productId).Update("stock_quantity", gorm.Expr("stock_quantity + ?", quantityChange)).Error; err != nil {
+		log.Err(err).Msg("Issue exist in UpdateStock")
+		return err
+	}
+	return nil
 }
