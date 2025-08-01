b7b1e019755c0a01c10ae6c2e85d69ab0b30d0d2
Pratyush <oggycandle786@gmail.com>
2025-08-01 23:57:06 +0530

    Updating imp

--- controller/order_controller.go
+++ controller/order_controller.go
@@ -1,11 +1,12 @@
 package controller
 
 import (
-	"encoding/json"
 	"github.com/pratyush934/sibling-bond-server/cjson"
-	"github.com/pratyush934/sibling-bond-server/dto"
 	"github.com/pratyush934/sibling-bond-server/models"
+	"github.com/rs/zerolog/log"
 	"net/http"
+	"strings"
+	"time"
 )
 
 /*
@@ -18,61 +19,135 @@ GetAllOrders - List all orders (admin only)
 
 func CreateOrder(w http.ResponseWriter, r *http.Request) {
 
-	/*
-		IT IS WRONG
-
-	*/
-
 	userId, ok := r.Context().Value("userId").(string)
 
 	if userId == "" || !ok {
 		panic(cjson.HTTPError{
 			Status:        http.StatusUnauthorized,
-			Message:       "Not found the UserId",
+			Message:       "There is no UserId, if there is we are not able to get it",
 			InternalError: nil,
 		})
 	}
 
-	var orderModel dto.OrderModel
-	if err := json.NewDecoder(r.Body).Decode(&orderModel); err != nil {
+	shippingAddressId := r.URL.Query().Get("shippingAddressId")
+	if shippingAddressId == "" {
 		panic(cjson.HTTPError{
 			Status:        http.StatusNotFound,
-			Message:       "Model is not there",
+			Message:       "There is not shipping Id found",
+			InternalError: nil,
+		})
+	}
+	paymentMethod := r.URL.Query().Get("paymentMethod")
+	if paymentMethod == "" {
+		paymentMethod = "cod"
+	}
+
+	cartByUserId, err := models.GetCartByUserId(userId)
+	if err != nil {
+		panic(cjson.HTTPError{
+			Status:        http.StatusNotFound,
+			Message:       "There are is not a single cart found",
 			InternalError: err,
 		})
 	}
 
-	newOrder := models.Order{
-		UserId:            userId,
-		ShippingAddressId: orderModel.ShippingAddressId,
-		TotalAmount:       orderModel.TotalAmount,
-		PaymentStatus:     orderModel.PaymentStatus,
-		Status:            orderModel.Status,
-		PaymentMode:       orderModel.PaymentMode,
-		TrackingNumber:    orderModel.TrackingNumber,
+	if len(cartByUserId.CartItems) == 0 {
+		panic(cjson.HTTPError{
+			Status:        http.StatusExpectationFailed,
+			Message:       "There are not Items in the cart",
+			InternalError: nil,
+		})
 	}
 
-	if len(orderModel.OrderItems) > 0 {
-		newOrderItems := make([]models.OrderItem, 0, len(orderModel.OrderItems))
-		for _, v := range orderModel.OrderItems {
-			newOrderItems = append(newOrderItems, models.OrderItem{
-				ProductId:       v.ProductId,
-				Quantity:        v.Quantity,
-				PriceAtPurchase: v.PriceAtPurchase,
-			})
+	orderItemsSlice := make([]models.OrderItem, 0, len(cartByUserId.CartItems))
+
+	totalAmount := 0
+	countItem := 0
+
+	for _, v := range cartByUserId.CartItems {
+
+		orderItem := models.OrderItem{
+			ProductId:       v.ProductId,
+			Quantity:        v.Quantity,
+			PriceAtPurchase: v.PriceAtAdding,
 		}
-		newOrder.OrderItems = newOrderItems
+
+		totalAmount += v.PriceAtAdding * v.Quantity
+		countItem += v.Quantity
+
+		orderItemsSlice = append(orderItemsSlice, orderItem)
 	}
 
-	createdOrder, err := newOrder.Create()
+	newOrderModel := models.Order{
+		UserId:            userId,
+		OrderItems:        orderItemsSlice,
+		TotalAmount:       totalAmount,
+		ShippingAddressId: shippingAddressId,
+		PaymentMode:       paymentMethod,
+		PaymentStatus:     "pending",
+		Status:            "pending",
+		TrackingNumber:    generateTrackingNumber(),
+	}
+
+	createdOrder, err := newOrderModel.Create()
 
 	if err != nil {
 		panic(cjson.HTTPError{
-			Status:        http.StatusBadRequest,
-			Message:       "Not able to create the Order",
+			Status:        http.StatusExpectationFailed,
+			Message:       "Not able to create Order",
 			InternalError: err,
 		})
 	}
+
+	err = models.DeleteCart(cartByUserId.Id)
+	if err != nil {
+		log.Err(err).Msg("not able to delete the cart but order is now created")
+	}
 	_ = cjson.WriteJSON(w, http.StatusCreated, createdOrder)
 
+}
+
+func CancelOrder(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func UpdateOrderStatus(w http.ResponseWriter, r *http.Request) {
+
+	role, ok := r.Context().Value("role").(int)
+
+	if role != 2 || !ok {
+		panic(cjson.HTTPError{
+			Status:        http.StatusUnauthorized,
+			Message:       "Not able to get the role or if got the person is not admin",
+			InternalError: nil,
+		})
+	}
+
+	orderStatus := r.URL.Query().Get("orderStatus")
+	if orderStatus == "" {
+		orderStatus = "delivered"
+	}
+	orderId := r.URL.Query().Get("orderId")
+	if orderId == "" {
+		panic(cjson.HTTPError{
+			Status:        http.StatusNotFound,
+			Message:       "please provide orderId",
+			InternalError: nil,
+		})
+	}
+
+	status, err := models.UpdateStatus(orderId, strings.ToLower(orderStatus))
+	if err != nil {
+		panic(cjson.HTTPError{
+			Status:        http.StatusInternalServerError,
+			Message:       "Not able to update the status",
+			InternalError: err,
+		})
+	}
+
+	_ = cjson.WriteJSON(w, http.StatusOK, status)
+}
+
+func generateTrackingNumber() int {
+	return (int)(time.Now().Unix() % 1000000)
 }
